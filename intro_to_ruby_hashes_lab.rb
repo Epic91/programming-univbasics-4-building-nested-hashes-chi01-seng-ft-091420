c663160899e54c5bda78bfa0995e2be4befe7583
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-31 06:05:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  new_hash = {:railroads => {}, :pieces => "4", :rent_in_dollars => {}, :names => {}}
+  new_hash = {:railroads => {:pieces => "4", :rent_in_dollars => {}, :names => {}}}
   new_hash
 end
 
