f1b397054f9ea47874d418d9f1024a2ea57adc9f
Evelyn Hernandez <hernandeze791@gmail.com>
2020-08-31 02:09:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,9 @@
 def base_hash
   {:railroads => "1"}
-  base_hash = {
-    :railroads => {
-      "1"
+  base_hash = {:railroads => {"1"
     }
-  
+  }
+
 
 
     # Remember implicit return! We're returning this new Hash _without_ the
